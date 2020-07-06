0b9e4d013518c8014587feb28ede03ab2143c55d
Javier Carmona Ferri <javcarfer@MacBook-Air-de-Cristina.local>
2020-07-06 22:04:39 +0200

    If for info

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ var app=new Vue({
         product:'Socks',
         image:'./assets/vmSocks-green.jpg',
         description:'This is a pair of socks',
-        link:'https://vuetifyjs.com/en/getting-started/quick-start/'
+        link:'https://vuetifyjs.com/en/getting-started/quick-start/',
+        inventory:100,
+        onSale:"true"
     }
 })
