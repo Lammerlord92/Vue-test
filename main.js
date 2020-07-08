60f4fc0d0457aed7a3f5f633805f249adaf9abe9
Javier Carmona Ferri <javcarfer@MacBook-Air-de-Cristina.local>
2020-07-08 18:38:53 +0200

    change img method

--- main.js
+++ main.js
@@ -1,29 +1,34 @@
-var product='Socks'
-var app=new Vue({
-    el:'#app',
-    data:{
-        product:'Socks',
-        image:'./assets/vmSocks-green.jpg',
-        description:'This is a pair of socks',
-        link:'https://vuetifyjs.com/en/getting-started/quick-start/',
-        inventory:100,
-        onSale:"true",
-        details:["80% cotton", "20% polyester", "Gender-neutral"],
-        variants:[
+var product = 'Socks'
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: './assets/vmSocks-green.jpeg',
+        description: 'This is a pair of socks',
+        link: 'https://vuetifyjs.com/en/getting-started/quick-start/',
+        inventory: 100,
+        onSale: "true",
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
             {
                 variantId: 2234,
-                variantColor: "green"
+                variantColor: "green",
+                variantImage: './assets/vmSocks-green.jpeg'
             },
             {
                 variantId: 2235,
-                variantColor: "blue"
+                variantColor: "blue",
+                variantImage: './assets/vmSocks-blue.jpeg'
             }
         ],
-        cart:0
+        cart: 0
     },
-    methods:{
-        addToCart: function(){
+    methods: {
+        addToCart: function () {
             this.cart += 1
+        },
+        updateProduct: function(variantImage){
+            this.image=variantImage
         }
     }
 })
