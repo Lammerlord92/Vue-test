cce5464b912af6777371e791f81989628d461dcb
JavCarFer <javcarfer92@gmail.com>
2020-07-17 13:18:58 +0200

    Created first component

--- main.js
+++ main.js
@@ -1,7 +1,51 @@
-var product = 'Socks'
-var app = new Vue({
-    el: '#app',
-    data: {
+Vue.component('product',{
+    props: {
+        premium:{
+            type: Boolean,
+            required:true
+        }
+    },
+    template:`
+        <div class="product row">
+            
+            <div class="product-image col-sm">
+                <a :href="link"><img :src="image" class="img-thumbnail"></a>
+                
+            </div>
+
+            <div class="product-info col-sm">
+                <h1>{{ title }}</h1>
+                <p v-if="onSale">On Sale!</p>
+                <p>Shipping: {{ shipping }}</p>
+                <ul>
+                    <li v-for="detail in details">{{detail}}</li>
+                </ul>
+                <div v-for="(variant,index) in variants" 
+                :key="variant.variantId"
+                class="color-box col-sm"
+                @mouseover="updateProduct(index)">
+                    <div class="col-sm" :style="{backgroundColor:variant.variantColor}">
+                        <p>{{variant.variantColor}}</p>
+                    </div>  
+                    <div class="col-sm">  
+                        <p v-if="variant.variantQuantity > 5">In stock</p>
+                        <p v-else-if="variant.variantQuantity <= 5 && variant.variantQuantity > 0">Almost sold out!</p>
+                        <p v-else>Out of stock</p>
+                        <button v-on:click="addToCart" 
+                                :disabled="!variantInStock(index)"
+                                :class="{disabledButton: !variant.variantQuantity>0}">
+                                Add to cart</button>
+                        <button v-if="variant.variantCart>0" v-on:click="removeFromCart">Remove from cart</button>
+                        <div class="variant.variantCart">
+                            <p>Cart ({{variant.variantCart}})</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    `,
+    data(){return{
         product: 'Socks',
         brand:"Vue Mastery",
         selectedVariant:0,
@@ -25,7 +69,7 @@ var app = new Vue({
                 variantCart:0
             }
         ]
-    },
+    }},
     methods: {
         addToCart() {
             this.variants[this.selectedVariant].variantCart += 1
@@ -49,6 +93,17 @@ var app = new Vue({
         },
         image() {
             return this.variants[this.selectedVariant].variantImage
+        },
+        shipping(){
+            if(this.premium) return "Free"
+            return "2,99"
         }
+    }
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        premium:false
     }
 })
